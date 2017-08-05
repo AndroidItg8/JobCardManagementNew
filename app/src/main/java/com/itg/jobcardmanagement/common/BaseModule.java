01f1d19ca7757d8b51deaa030dab1c793e874480
itgAndroid <app.itechgalaxy@gmail.com>
2017-08-05 18:43:01 +0530

    bbs

---
+++ app/src/main/java/com/itg/jobcardmanagement/common/BaseModule.java
@@ -0,0 +1,11 @@
+package com.itg.jobcardmanagement.common;
+
+/**
+ * Created by itg_Android on 8/5/2017.
+ */
+
+public interface BaseModule {
+    void onPause();
+    void onResume();
+    void onDestroy();
+}
---
+++ app/src/main/java/com/itg/jobcardmanagement/registration/mvp/LoginPresenterImp.java
@@ -0,0 +1,120 @@
+package com.itg.jobcardmanagement.registration.mvp;
+
+import com.itg.jobcardmanagement.common.BaseWeakPresenter;
+
+/**
+ * Created by itg_Android on 8/5/2017.
+ */
+
+public class LoginPresenterImp extends BaseWeakPresenter implements LoginRegMVP.LoginListener, LoginRegMVP.LoginPresenter {
+
+
+    public static final int EMAIL = 1;
+    public static final int MOBILE = 2;
+    private LoginModuleImp module;
+
+    public LoginPresenterImp(LoginRegMVP.LoginView view) {
+        super(view);
+        try {
+            module = new LoginModuleImp(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUsernameSubmit(String username) {
+        if (hasView()) {
+            int type = checkTypeofUsername(username);
+            if (type > 0) {
+                module.onUsernameSubmit(username,type);
+            }
+        }
+    }
+
+    private int checkTypeofUsername(String username) {
+        int type = -1;
+        if (isEmailValid(username)) {
+            type = EMAIL;
+            getLoginView().onEmailEntered(username);
+        } else if (isValidPhoneNumber(username)) {
+            if (username.length() != 10) {
+                getLoginView().onMobileInvalid();
+            } else {
+                type = MOBILE;
+                getLoginView().onMobileNumberEntered(username);
+            }
+        } else {
+            getLoginView().onEmailInvalid();
+        }
+        return type;
+    }
+
+    private boolean isValidPhoneNumber(String mobile) {
+        String regEx = "^[0-9]{11,12}$";
+        return mobile.matches(regEx);
+    }
+
+    private boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    @Override
+    public void onSignupClicked(String username) {
+
+    }
+
+    @Override
+    public void onRegistrationClicked(Object regModel) {
+
+    }
+
+    @Override
+    public void onLoginClicked(String userId, String password) {
+
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public void onUserAvaiable(String userId, String profilePicUrl) {
+
+    }
+
+    @Override
+    public void onUserFail(String message) {
+
+    }
+
+    @Override
+    public void onError(String err) {
+
+    }
+
+    @Override
+    public void onRegFail(String message) {
+
+    }
+
+    @Override
+    public void onRegSuccess(Object module) {
+
+    }
+
+    private LoginRegMVP.LoginView getLoginView() {
+        return (LoginRegMVP.LoginView) getView();
+    }
+}
