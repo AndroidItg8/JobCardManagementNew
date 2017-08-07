143defdb0fc3a2b09acdd22e8c953451a662e976
itgAndroid <app.itechgalaxy@gmail.com>
2017-08-07 18:06:35 +0530

    volley

---
+++ app/src/main/java/com/itg/jobcardmanagement/common/NetworkListener.java
@@ -0,0 +1,10 @@
+package com.itg.jobcardmanagement.common;
+
+/**
+ * Created by itg_Android on 8/7/2017.
+ */
+
+public interface NetworkListener {
+    String LOGIN_URL="/Account/VerifyUser";
+    String USER_LOGIN="/Account/UserLogin";
+}
